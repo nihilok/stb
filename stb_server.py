bf170b4d6be243cc597384e72553d4fe2b99aad3
nihilok <70285461+nihilok@users.noreply.github.com>
2021-04-07 12:27:24 +0100

    running score

--- stb_server.py
+++ stb_server.py
@@ -100,7 +100,7 @@ class GameEngine:
 
     def parse_score_tally(self):
         if self.score_tally < 0:
-            return f'Team A has won {self.score_tally} more {"game" if self.score_tally == 1 else "games"} more than Team B'
+            return f'Team A has won {self.score_tally} more {"game" if self.score_tally == -1 else "games"} more than Team B'
         elif self.score_tally > 0:
             return f'Team B has won {self.score_tally} more {"game" if self.score_tally == 1 else "games"} more than Team A'
         return "The scores are currently level"
