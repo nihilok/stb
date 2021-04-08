55b4ecdcd6e32b344243caa3b9a12ab0156488fa
nihilok <t0k3n8l4ck@gmail.com>
2021-04-08 07:43:11 +0100

    start script

--- run_server.sh
+++ run_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 clear ;
-echo "Starting STOP THE BUS server..."
+echo "Starting STOP THE BUS server...";
 source /mnt/f/Coding/stb/wsl/venv/bin/activate ;
 gunicorn -k eventlet -w 1 --bind 0.0.0.0:7777 stb_server:app
