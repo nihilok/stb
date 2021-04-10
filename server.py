c123c16115fae32b3598386bd81679514453f335
nihilok <t0k3n8l4ck@gmail.com>
2021-04-10 15:16:00 +0100

    cors wss

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ from utils import random_string, refresh_cookies, riffle, return_user_dict, chec
 app = Flask(__name__)
 app.secret_key = random_string(24)
 CORS(app)
-socketio = SocketIO(app, cors_allowed_origins=['http://185.3.94.188:7777'])
+socketio = SocketIO(app, cors_allowed_origins=['http://stb.smarthome.com'])
 games = {}
 
 
